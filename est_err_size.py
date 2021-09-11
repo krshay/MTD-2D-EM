16ba2e7c5a803eae5c5bbf29b2132fa67e6fc574
Shay Kreymer <56560602+krshay@users.noreply.github.com>
2021-09-11 20:36:31 +0300

    Update est_err_size.py

--- est_err_size.py
+++ est_err_size.py
@@ -32,16 +32,22 @@ if __name__ == '__main__':
     pool.join() 
     
     errs_EM = np.zeros((Niters, Nsizes))
+    times_EM = np.zeros((Niters, Nsizes))
 
     for j in range(Niters):
         errs_EM[j, :] = S[j][0][np.arange(Nsizes), np.argmin(S[j][1], axis=1)]
+        times_EM[j, :] = S[j][2][np.arange(Nsizes), np.argmin(S[j][1], axis=1)]
     errs_EM_mean = np.mean(errs_EM, 0)
+    times_EM_mean = np.mean(times_EM, 0)
     
     errs_ac = np.zeros((Niters, Nsizes))
+    times_ac = np.zeros((Niters, Nsizes))
 
     for j in range(Niters):
-        errs_ac[j, :] = S[j][2][np.arange(Nsizes), np.argmin(S[j][3], axis=1)]
+        errs_ac[j, :] = S[j][3][np.arange(Nsizes), np.argmin(S[j][4], axis=1)]
+        times_ac[j, :] = S[j][5][np.arange(Nsizes), np.argmin(S[j][4], axis=1)]
     errs_ac_mean = np.mean(errs_ac, 0)
+    times_ac_mean = np.mean(times_ac, 0)
     
     # %% plots
     plt.close("all")
@@ -49,15 +55,29 @@ if __name__ == '__main__':
         fig = plt.figure()
         
         plt.loglog(sizes**2, errs_EM_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
-        plt.loglog(sizes**2, errs_EM_mean, '.-b', label=r'EM $\xi$ and $\zeta$')
+        plt.loglog(sizes**2, errs_EM_mean, '.-b', label=r'EM')
     
         plt.loglog(sizes**2, errs_ac_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
-        plt.loglog(sizes**2, errs_ac_mean, '.--r', label='Algorithm 1')
+        plt.loglog(sizes**2, errs_ac_mean, '.--r', label='Autocorrelation analysis')
         
         plt.legend(loc=(0.5, 0.62))#, fontsize=6)
         
         plt.xlabel('Measurement size [pixels]')
         
         plt.ylabel('Mean estimation error')
+        fig.tight_layout()
+        plt.show()
+        
+        fig = plt.figure()
+
+        plt.semilogx(sizes**2, errs_EM_mean, '.-b', label=r'EM')
+
+        plt.semilogx(sizes**2, errs_ac_mean, '.--r', label='Autocorrelation analysis')
+        
+        plt.legend(loc=(0.5, 0.62))#, fontsize=6)
+        
+        plt.xlabel('Measurement size [pixels]')
+        
+        plt.ylabel('Mean computation time [CPU sec]')
         fig.tight_layout()
         plt.show()
