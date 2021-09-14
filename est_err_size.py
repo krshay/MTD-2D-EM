3e92c68865a1259c880e9df930e6097675c8a94b
Shay Kreymer <kreymershay@gmail.com>
2021-09-14 20:05:29 +0300

    Update est_err_size.py

--- est_err_size.py
+++ est_err_size.py
@@ -26,56 +26,56 @@ if __name__ == '__main__':
     num_cpus = mp.cpu_count()
     num_cpus = 5
     # %% EM and Autocorrelation Analysis
-    # pool = mp.Pool(num_cpus)
-    # S = pool.starmap(calc_err_size_both, [[L, ne, sizes, SNR, gamma, K, i] for i in range(Niters)])
-    # pool.close()
-    # pool.join() 
+    pool = mp.Pool(num_cpus)
+    S = pool.starmap(calc_err_size_both, [[L, ne, sizes, SNR, gamma, K, i] for i in range(Niters)])
+    pool.close()
+    pool.join() 
     
-    # errs_EM = np.zeros((Niters, Nsizes))
-    # times_EM = np.zeros((Niters, Nsizes))
+    errs_EM = np.zeros((Niters, Nsizes))
+    times_EM = np.zeros((Niters, Nsizes))
 
-    # for j in range(Niters):
-    #     errs_EM[j, :] = S[j][0][np.arange(Nsizes), np.argmin(S[j][1], axis=1)]
-    #     times_EM[j, :] = S[j][2][np.arange(Nsizes), np.argmin(S[j][1], axis=1)]
-    # errs_EM_mean = np.mean(errs_EM, 0)
-    # times_EM_mean = np.mean(times_EM, 0)
+    for j in range(Niters):
+        errs_EM[j, :] = S[j][0][np.arange(Nsizes), np.argmin(S[j][1], axis=1)]
+        times_EM[j, :] = S[j][2][np.arange(Nsizes), np.argmin(S[j][1], axis=1)]
+    errs_EM_mean = np.mean(errs_EM, 0)
+    times_EM_mean = np.mean(times_EM, 0)
     
-    # errs_ac = np.zeros((Niters, Nsizes))
-    # times_ac = np.zeros((Niters, Nsizes))
+    errs_ac = np.zeros((Niters, Nsizes))
+    times_ac = np.zeros((Niters, Nsizes))
 
-    # for j in range(Niters):
-    #     errs_ac[j, :] = S[j][3][np.arange(Nsizes), np.argmin(S[j][4], axis=1)]
-    #     times_ac[j, :] = S[j][5][np.arange(Nsizes), np.argmin(S[j][4], axis=1)]
-    # errs_ac_mean = np.mean(errs_ac, 0)
-    # times_ac_mean = np.mean(times_ac, 0)
-    errs_EM = np.zeros((Niters, Nsizes, 1))
-    costs_EM = np.zeros((Niters, Nsizes, 1))
-    times_EM = np.zeros((Niters, Nsizes, 1))
-    errs_ac = np.zeros((Niters, Nsizes, 1))
-    costs_ac = np.zeros((Niters, Nsizes, 1))
-    times_ac = np.zeros((Niters, Nsizes, 1))
+    for j in range(Niters):
+        errs_ac[j, :] = S[j][3][np.arange(Nsizes), np.argmin(S[j][4], axis=1)]
+        times_ac[j, :] = S[j][5][np.arange(Nsizes), np.argmin(S[j][4], axis=1)]
+    errs_ac_mean = np.mean(errs_ac, 0)
+    times_ac_mean = np.mean(times_ac, 0)
+    # errs_EM = np.zeros((Niters, Nsizes, 1))
+    # costs_EM = np.zeros((Niters, Nsizes, 1))
+    # times_EM = np.zeros((Niters, Nsizes, 1))
+    # errs_ac = np.zeros((Niters, Nsizes, 1))
+    # costs_ac = np.zeros((Niters, Nsizes, 1))
+    # times_ac = np.zeros((Niters, Nsizes, 1))
     
-    for i in range((Niters)):
-        errs_EM[i, :, :], costs_EM[i, :, :], times_EM[i, :, :], errs_ac[i, :, :], costs_ac[i, :, :], times_ac[i, :, :] = calc_err_size_both(L, ne, sizes, SNR, gamma, K, i)
+    # for i in range((Niters)):
+    #     errs_EM[i, :, :], costs_EM[i, :, :], times_EM[i, :, :], errs_ac[i, :, :], costs_ac[i, :, :], times_ac[i, :, :] = calc_err_size_both(L, ne, sizes, SNR, gamma, K, i)
     
-    # # %% plots
-    # plt.close("all")
-    # with plt.style.context('ieee'):
-    #     fig = plt.figure()
+    # %% plots
+    plt.close("all")
+    with plt.style.context('ieee'):
+        fig = plt.figure()
         
-    #     plt.loglog(sizes**2, errs_EM_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
-    #     plt.loglog(sizes**2, errs_EM_mean, '.-b', label=r'EM')
+        plt.loglog(sizes**2, errs_EM_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
+        plt.loglog(sizes**2, errs_EM_mean, '.-b', label=r'EM')
     
-    #     plt.loglog(sizes**2, errs_ac_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
-    #     plt.loglog(sizes**2, errs_ac_mean, '.--r', label='Autocorrelation analysis')
+        plt.loglog(sizes**2, errs_ac_mean[3]*(sizes**2/sizes[3]**2)**(-1/2), 'k--', label='_nolegend_', lw=0.5)
+        plt.loglog(sizes**2, errs_ac_mean, '.--r', label='Autocorrelation analysis')
         
-    #     plt.legend(loc=(0.5, 0.62))#, fontsize=6)
+        plt.legend(loc=(0.5, 0.62))#, fontsize=6)
         
-    #     plt.xlabel('Measurement size [pixels]')
+        plt.xlabel('Measurement size [pixels]')
         
-    #     plt.ylabel('Mean estimation error')
-    #     fig.tight_layout()
-    #     plt.show()
+        plt.ylabel('Mean estimation error')
+        fig.tight_layout()
+        plt.show()
         
     #     fig = plt.figure()
 
